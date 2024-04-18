54e60dc2ceceba1620e3614aa74b4f3a75733c3c
j-rxdriguez <92170960+j-rxdriguez@users.noreply.github.com>
2024-04-18 18:57:29 +0100

    Add files via upload
    Latest version

---
+++ loginDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginFlowInMauiBlazorApp.Models.DTO
+{
+    public class loginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
